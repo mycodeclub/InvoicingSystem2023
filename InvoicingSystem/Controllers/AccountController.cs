041213fbe8a0669d53878acad9ccb5288b62f8f4
AnkitSahay <ankitsahay1990@gmail.com>
2023-11-01 14:01:41 +0530

    initial commit

---
+++ InvoicingSystem/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
