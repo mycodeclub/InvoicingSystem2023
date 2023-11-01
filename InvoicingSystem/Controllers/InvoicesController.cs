1cf1ae76f024fb88efbd5e88d599654272100d02
AnkitSahay <ankitsahay1990@gmail.com>
2023-11-01 15:08:33 +0530

    Added Migration

---
+++ InvoicingSystem/Controllers/InvoicesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InvoicingSystem.DTO;
+using InvoicingSystem.EFCore;
+
+namespace InvoicingSystem.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public InvoicesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Invoices
+        public async Task<IActionResult> Index()
+        {
+              return _context.Invoices != null ? 
+                          View(await _context.Invoices.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Invoices'  is null.");
+        }
+
+        // GET: Invoices/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Invoices == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,InvoiceNumber")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                invoice.ID = Guid.NewGuid();
+                _context.Add(invoice);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Invoices == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ID,InvoiceNumber")] Invoice invoice)
+        {
+            if (id != invoice.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invoice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceExists(invoice.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Invoices == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Invoices == null)
+            {
+                return Problem("Entity set 'AppDbContext.Invoices'  is null.");
+            }
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice != null)
+            {
+                _context.Invoices.Remove(invoice);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvoiceExists(Guid id)
+        {
+          return (_context.Invoices?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
